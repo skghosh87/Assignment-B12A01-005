9a74029bb5cc0b2d5d21be1fc485914686729cd0
skghosh87 <sarghosh@gmail.com>
2025-08-29 00:29:08 +0600

    Heart Icon js added

--- code.js
+++ code.js
@@ -1,11 +1,16 @@
-const cardHeartIcon = document.getElementById("btn-heart-icon");
-const cardLoveCount = parseInt(
-  document.getElementById("heart-count").innerText
-);
-cardHeartIcon.addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log("Icon Clicked");
-  console.log(cardLoveCount);
-  const updateCardLoveCount = cardLoveCount + 1;
-  document.getElementById("heart-count").innerText = updateCardLoveCount;
-});
+// Navbar Heart Counter
+const cardLoveCount = document.getElementById("heart-count");
+
+// // All Heart Card Select
+const cardHeartIcon = document.querySelectorAll("#btn-heart-icon");
+
+for (cardIcon of cardHeartIcon) {
+  cardIcon.addEventListener("click", function (e) {
+    e.preventDefault();
+    const updateCardLoveCount = parseInt(cardLoveCount.innerText);
+    cardLoveCount.innerText = updateCardLoveCount + 1;
+  });
+}
+
+const coinCount = document.getElementById("coin-count");
+const callingButton = document.querySelectorAll("#btn-call");
